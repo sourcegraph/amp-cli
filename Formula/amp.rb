baad7daea1bfeec1ca28841b89fbba7cb02cef73
Amp <amp@ampcode.com>
2025-07-31 18:29:03 +0000

    Update Homebrew formula to v0.0.1753985789-gda0d4e

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,27 +4,27 @@
 class Amp < Formula
   desc 'Amp CLI - AI-powered coding assistant'
   homepage 'https://github.com/sourcegraph/amp-packages'
-  url '        https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-arm64'
-  sha256 'REPLACE_WITH_PLACEHOLDER'
+  url '          https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-arm64'
+  sha256 'da71ce4c2822c6827a5ea71ab031b4c112dcaa1a8d4002129cbbec14ae2bf42c'
   version '0.0.1753985789-gda0d4e'
 
   on_macos do
     if Hardware::CPU.arm?
-      url '        https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-arm64'
-      sha256 'REPLACE_WITH_PLACEHOLDER'
+      url '          https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-arm64'
+      sha256 'da71ce4c2822c6827a5ea71ab031b4c112dcaa1a8d4002129cbbec14ae2bf42c'
     else
-      url '        https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-x64'
-      sha256 'REPLACE_WITH_PLACEHOLDER'
+      url '          https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-darwin-x64'
+      sha256 'a7d60dce6a5d17f4e7442a58e1354c489d38b1a03908cda642404bcc337a73e6'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url '        https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-linux-arm64'
-      sha256 'REPLACE_WITH_PLACEHOLDER'
+      url '          https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-linux-arm64'
+      sha256 '64a985094e21ebb1306a56da5a2b443f19fe629f489bf9840eef14edc2d073df'
     else
-      url '        https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-linux-x64'
-      sha256 'REPLACE_WITH_PLACEHOLDER'
+      url '          https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753985789-gda0d4e/amp-linux-x64'
+      sha256 'd1fa512f3e6ab2a82ac8886746cd7549231cd7aaf6d7db05352c211e6408cf9b'
     end
   end
 

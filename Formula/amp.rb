412bf105ef15baa656259e1244d1ddb6e4f26685
Amp <amp@ampcode.com>
2025-07-31 16:12:10 +0000

    Update Homebrew formula to v0.0.1753977981-g14e5bc

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,27 +4,27 @@
 class Amp < Formula
   desc 'Amp CLI - AI-powered coding assistant'
   homepage 'https://github.com/sourcegraph/amp-packages'
-  url 'https://github.com/sourcegraph/amp-packages/releases/download/vVERSION_PLACEHOLDER/amp-darwin-arm64'
-  sha256 'REPLACE_WITH_DARWIN_ARM64_SHA256'
-  version 'VERSION_PLACEHOLDER'
+  url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
+  sha256 ''
+  version '0.0.1753977981-g14e5bc'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/vVERSION_PLACEHOLDER/amp-darwin-arm64'
-      sha256 'REPLACE_WITH_DARWIN_ARM64_SHA256'
+      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
+      sha256 ''
     else
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/vVERSION_PLACEHOLDER/amp-darwin-x64'
-      sha256 'REPLACE_WITH_DARWIN_AMD64_SHA256'
+      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-x64'
+      sha256 '73c1e9b46aba31ce2ff938b2d5d19a01d76338241b40cfa225a11d9d88761e77'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/vVERSION_PLACEHOLDER/amp-linux-arm64'
-      sha256 'REPLACE_WITH_LINUX_ARM64_SHA256'
+      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-linux-arm64'
+      sha256 'REPLACE_WITH_PLACEHOLDER'
     else
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/vVERSION_PLACEHOLDER/amp-linux-x64'
-      sha256 'REPLACE_WITH_LINUX_AMD64_SHA256'
+      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-linux-x64'
+      sha256 'REPLACE_WITH_PLACEHOLDER'
     end
   end
 

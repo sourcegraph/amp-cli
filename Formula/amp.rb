c919db89291b258157c1522456b043855c7c08fd
Amp <amp@ampcode.com>
2025-07-31 17:30:57 +0000

    Update Homebrew formula to v0.0.1753982703-gda0d4e

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,26 +4,26 @@
 class Amp < Formula
   desc 'Amp CLI - AI-powered coding assistant'
   homepage 'https://github.com/sourcegraph/amp-packages'
-  url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
-  sha256 'fdde66b16860a824fe59d5eb8961ed8b5c16dcb717b43a1078e27278821fecec'
+  url '  https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753982703-gda0d4e/amp-darwin-arm64'
+  sha256 ''
   version '0.0.1753977981-g14e5bc'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
-      sha256 'fdde66b16860a824fe59d5eb8961ed8b5c16dcb717b43a1078e27278821fecec'
+      url '  https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753982703-gda0d4e/amp-darwin-arm64'
+      sha256 ''
     else
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-x64'
-      sha256 '349ac473187ae27466708e7e1aa4d3ad3fe9ed0343bedb5c930bc3a5bb2a853e'
+      url '  https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753982703-gda0d4e/amp-darwin-x64'
+      sha256 ''
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-linux-arm64'
+      url '  https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753982703-gda0d4e/amp-linux-arm64'
       sha256 'REPLACE_WITH_PLACEHOLDER'
     else
-      url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-linux-x64'
+      url '  https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753982703-gda0d4e/amp-linux-x64'
       sha256 'REPLACE_WITH_PLACEHOLDER'
     end
   end

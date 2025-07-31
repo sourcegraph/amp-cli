a02893510f6386ebe00a1090810ec82681596966
Amp <amp@ampcode.com>
2025-07-31 16:13:33 +0000

    Update Homebrew formula to v0.0.1753977981-g14e5bc

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -5,13 +5,13 @@ class Amp < Formula
   desc 'Amp CLI - AI-powered coding assistant'
   homepage 'https://github.com/sourcegraph/amp-packages'
   url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
-  sha256 ''
+  sha256 '819e893b8eec9ba3240f93ff231a96fe471f183844be3cd02b2c4b4c9cfe8225'
   version '0.0.1753977981-g14e5bc'
 
   on_macos do
     if Hardware::CPU.arm?
       url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-arm64'
-      sha256 ''
+      sha256 '819e893b8eec9ba3240f93ff231a96fe471f183844be3cd02b2c4b4c9cfe8225'
     else
       url 'https://github.com/sourcegraph/amp-packages/releases/download/v0.0.1753977981-g14e5bc/amp-darwin-x64'
       sha256 '73c1e9b46aba31ce2ff938b2d5d19a01d76338241b40cfa225a11d9d88761e77'

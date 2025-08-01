d9175d9bb435e020535509acdc5d7dc223bd80ba
Amp <amp@ampcode.com>
2025-08-01 12:24:01 +0000

    Update Homebrew formula to v0.0.1754050083

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,27 +4,27 @@
 class Amp < Formula
   desc 'Amp CLI - An agentic coding tool, in research preview from Sourcegraph'
   homepage 'https://ampcode.com'
-  url 'https://packages.ampcode.com/binaries/v0.0.1754035600/amp-darwin-arm64'
-  sha256 'c98dc23b96f8abfd10a9c30e91cf4e2128ed00f4f1419f80109598b873f17e31'
-  version '0.0.1754035600'
+  url 'https://packages.ampcode.com/binaries/v0.0.1754050083/amp-darwin-arm64'
+  sha256 '1d3eae41dcd8476b3eaf2a47627a735ea14816b5304bf0dafc88699d06d93357'
+  version '0.0.1754050083'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://packages.ampcode.com/binaries/v0.0.1754035600/amp-darwin-arm64'
-      sha256 'c98dc23b96f8abfd10a9c30e91cf4e2128ed00f4f1419f80109598b873f17e31'
+      url 'https://packages.ampcode.com/binaries/v0.0.1754050083/amp-darwin-arm64'
+      sha256 '1d3eae41dcd8476b3eaf2a47627a735ea14816b5304bf0dafc88699d06d93357'
     else
-      url 'https://packages.ampcode.com/binaries/v0.0.1754035600/amp-darwin-x64'
-      sha256 'dbe57d76d6c19322fbfe506af6d207577d98af41f5824161aeb8b4a3465de33b'
+      url 'https://packages.ampcode.com/binaries/v0.0.1754050083/amp-darwin-x64'
+      sha256 'af846e94968ba7eaa05ee865427c07047a9fbe12c425986e038e9f9c0b3810d9'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url 'https://packages.ampcode.com/binaries/v0.0.1754035600/amp-linux-arm64'
-      sha256 '6bb51ffb56e7aa078932f65d4e9b419102c726caf098ffc1aa924af7595e36bd'
+      url 'https://packages.ampcode.com/binaries/v0.0.1754050083/amp-linux-arm64'
+      sha256 '167e5ebaace8e6d57175240cd387c2222d947339f9f74c9855bfc38f8ff109e5'
     else
-      url 'https://packages.ampcode.com/binaries/v0.0.1754035600/amp-linux-x64'
-      sha256 '953bd1991e6baddd5025ad8c0c985d350dc808cc0c586c589d313e8302b43868'
+      url 'https://packages.ampcode.com/binaries/v0.0.1754050083/amp-linux-x64'
+      sha256 '31a8bbdcb345bac6fea68c2fdb86f3c874818069335f7fb04812d4e4bf2e6b90'
     end
   end
 

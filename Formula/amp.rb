289178fe107eab8a5a7f3520690c6b74f260d34d
Amp <amp@ampcode.com>
2025-08-01 02:40:27 +0000

    Update Homebrew formula to v0.0.1754015686

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,27 +4,27 @@
 class Amp < Formula
   desc 'Amp CLI - An agentic coding tool, in research preview from Sourcegraph'
   homepage 'https://github.com/sourcegraph/amp-cli'
-  url '                https://github.com/sourcegraph/amp-cli/releases/download/vVERSION_PLACEHOLDER/amp-darwin-arm64'
-  sha256 'REPLACE_WITH_DARWIN_ARM64_SHA256'
-  version 'VERSION_PLACEHOLDER'
+  url '                https://github.com/sourcegraph/amp-cli/releases/download/v0.0.1754015686/amp-darwin-arm64'
+  sha256 ''
+  version '0.0.1754015686'
 
   on_macos do
     if Hardware::CPU.arm?
-      url '                https://github.com/sourcegraph/amp-cli/releases/download/vVERSION_PLACEHOLDER/amp-darwin-arm64'
-      sha256 'REPLACE_WITH_DARWIN_ARM64_SHA256'
+      url '                https://github.com/sourcegraph/amp-cli/releases/download/v0.0.1754015686/amp-darwin-arm64'
+      sha256 ''
     else
-      url '                https://github.com/sourcegraph/amp-cli/releases/download/vVERSION_PLACEHOLDER/amp-darwin-x64'
-      sha256 'REPLACE_WITH_DARWIN_AMD64_SHA256'
+      url '                https://github.com/sourcegraph/amp-cli/releases/download/v0.0.1754015686/amp-darwin-x64'
+      sha256 ''
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url '                https://github.com/sourcegraph/amp-cli/releases/download/vVERSION_PLACEHOLDER/amp-linux-arm64'
-      sha256 'REPLACE_WITH_LINUX_ARM64_SHA256'
+      url '                https://github.com/sourcegraph/amp-cli/releases/download/v0.0.1754015686/amp-linux-arm64'
+      sha256 'ec7753651b14628a7fed9e899ecf872ab90bf9f5f7b9de4163e9f7950f913236'
     else
-      url '                https://github.com/sourcegraph/amp-cli/releases/download/vVERSION_PLACEHOLDER/amp-linux-x64'
-      sha256 'REPLACE_WITH_LINUX_AMD64_SHA256'
+      url '                https://github.com/sourcegraph/amp-cli/releases/download/v0.0.1754015686/amp-linux-x64'
+      sha256 '5a6b8c0222e4bf43cd138cd1d6ddc8e53313afe5b35ede798dfcaa9fc7a15be2'
     end
   end
 

9f9128a094cd51945191092ec964eb5f260d6188
Amp <amp@ampcode.com>
2025-08-01 04:42:37 +0000

    Update Nix flake to v0.0.1754022453

--- flake.nix
+++ flake.nix
@@ -21,14 +21,14 @@
 
         arch = archMap.${system} or (throw "Unsupported system: ${system}");
 
-        version = "0.0.1754015686";
+        version = "0.0.1754022453";
 
         # These will be updated with actual SHA256 hashes by the workflow
         shaMap = {
-          "linux-x64" = "5a6b8c0222e4bf43cd138cd1d6ddc8e53313afe5b35ede798dfcaa9fc7a15be2";
-          "linux-arm64" = "ec7753651b14628a7fed9e899ecf872ab90bf9f5f7b9de4163e9f7950f913236";
-          "darwin-x64" = "1a6820c140916057853196fd102a0495f84c02da6851301cb11261242c41ae8e";
-          "darwin-arm64" = "fa1c72ec69a0daf37c0b1e25dcbb2a0b0d54130d9ece1cde3606775ea8038f25";
+          "linux-x64" = "aabef5e6484aed7d2226f23160a3e08e98a0a9fe178f47c10ac25decde966f4e";
+          "linux-arm64" = "213222afe829b05b0e1b6f16b5dd7ccafeb9a6ca2b32d43115f2eb4fb98ffd2b";
+          "darwin-x64" = "7b700501c9dddb8a10dbad36a180576a6eb0fb63bdd94eabcc3537a2d3bd8151";
+          "darwin-arm64" = "76168332b6cc793050bd71bf5357df6cc805819489ab617bbf1229cdeb198340";
         };
 
       in

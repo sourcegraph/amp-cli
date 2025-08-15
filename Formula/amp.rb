bd6eab20bdcef5efe164c354b1efba431d3a9d72
Amp <amp@ampcode.com>
2025-08-15 08:15:17 +0000

    Update Homebrew formula to v0.0.1755245184

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -4,27 +4,27 @@
 class Amp < Formula
   desc 'Amp CLI - An agentic coding tool, in research preview from Sourcegraph'
   homepage 'https://ampcode.com'
-  url 'https://packages.ampcode.com/binaries/cli/v0.0.1755231076/amp-darwin-arm64.zip'
-  sha256 '088563ce12eb2c358ef5b8b03e7dffb9a5fd05175daa96ddea8543784a643d19'
-  version '0.0.1755231076'
+  url 'https://packages.ampcode.com/binaries/cli/v0.0.1755245184/amp-darwin-arm64.zip'
+  sha256 '85105f892af87c510567a4ce031e7b5b03f670ef3b56a602cd78d47c37123ab3'
+  version '0.0.1755245184'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755231076/amp-darwin-arm64.zip'
-      sha256 '088563ce12eb2c358ef5b8b03e7dffb9a5fd05175daa96ddea8543784a643d19'
+      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755245184/amp-darwin-arm64.zip'
+      sha256 '85105f892af87c510567a4ce031e7b5b03f670ef3b56a602cd78d47c37123ab3'
     else
-      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755231076/amp-darwin-x64.zip'
-      sha256 'ad8584983075a083e5c6a58be9b545e1fec7a0298d69cb6eb4c6f6611170f3fc'
+      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755245184/amp-darwin-x64.zip'
+      sha256 '21a072a76360ba77bf91150cf53b8c63a04c6dc0fd9ea9b7cfab77f738283942'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755231076/amp-linux-arm64'
-      sha256 '7c90cbb0c332bde73613d2d1bcd30a1e0e53863f9a91cb3b9634d08b79ac2b89'
+      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755245184/amp-linux-arm64'
+      sha256 '4709b0d7ca160b5e7cb3729385d17aa06892aabc2d9f6f6582a4acd1ff426039'
     else
-      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755231076/amp-linux-x64'
-      sha256 '2db9c4cf7411b45e14eb6359ef1dcbe0adafef3e5246788f6cfafb4c49c02a9a'
+      url 'https://packages.ampcode.com/binaries/cli/v0.0.1755245184/amp-linux-x64'
+      sha256 'b187722ca84da20f5b77976d1780323f74aad07840f7b3dc0ba154046a1f637f'
     end
   end
 

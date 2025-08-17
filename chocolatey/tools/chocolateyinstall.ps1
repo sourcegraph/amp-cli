2e6f69e22809bf464e29e5a25ea19ecbdc27e15a
Amp <amp@ampcode.com>
2025-08-17 08:15:47 +0000

    Update Chocolatey package to v0.0.1755417945

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'amp'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://packages.ampcode.com/binaries/cli/v0.0.1755404022/amp-windows-x64.exe'
-$checksum64 = '7b2ec1b24fcf304bc107bfa77ce550bb8d92e2c16922c298b5e7b60db8786ae0'
+$url64 = 'https://packages.ampcode.com/binaries/cli/v0.0.1755417945/amp-windows-x64.exe'
+$checksum64 = 'b96a3112c9a1fa22003690cf7bf21075dd04f8ec7d99a4ac610610b03dc44152'
 
 $packageArgs = @{
   packageName   = $packageName
